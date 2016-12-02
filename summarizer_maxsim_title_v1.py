f4ff134b1715096a7972f916f1820ad59472751e
Gaurangi Raul <gaurangr@usc.edu>
2016-12-01 23:26:21 -0800

    Add files via upload

---
+++ summarizer_maxsim_title_v1.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 23:12:54 2016
+
+@author: Gaurangi Raul
+"""
+
+import os
+import mimetypes
+import re
+import operator
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+    
+def preprocess(sentence):
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(sentence)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    #print(filtered_sentence)
+    ps = PorterStemmer()
+    stemmed_sentence = [ps.stem(w) for w in filtered_sentence]
+    #print(stemmed_sentence)
+    preprocess_sentence = ' '.join([w for w in stemmed_sentence])
+    return preprocess_sentence
+    
+#extract sentences similar to max sentences based on TF-IDF scores
+def summarize_maxsim(filename):
+    input_file = open(filename, "r", encoding="latin1")
+    file_text = input_file.read()
+    input_file.close()
+    docs = re.split('\. |\? |! |\n',file_text)
+    for sent in docs:
+        if sent == '':
+            docs.remove(sent)
+    
+    documents = []
+    for sent in docs:        
+        documents.append(preprocess(sent))
+    print("Title:", documents[0])
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+    #print(tfidf_matrix.shape)
+    #print(tfidf_matrix.shape[0])
+    sim_scores = cosine_similarity(tfidf_matrix[0:tfidf_matrix.shape[0]], tfidf_matrix)
+    scores_dict = {}
+    for i in range(1, tfidf_matrix.shape[0]):
+        scores_sum = 0
+        for j in range(0, tfidf_matrix.shape[0]):
+            if sim_scores[i][j] > 0:
+                scores_sum += sim_scores[i][j]
+        scores_dict[i]=scores_sum
+    
+    #similarity with title    
+    title_dict = {}
+    for i in range(1, tfidf_matrix.shape[0]):
+        title_dict[i] = sim_scores[0][i]
+    
+    #combine similarity with title and other sentences    
+    final_dict = {}
+    for key in scores_dict:
+        final_dict[key] = (0.025 * scores_dict[key]) + (0.975 * title_dict[key])
+    sorted_final = sorted(final_dict.items(), key=operator.itemgetter(1), reverse=True)
+    
+    #print(sorted_scores)
+    #limit = min(int(0.05*len(scores_dict)), 5)
+    limit = int(0.1*len(scores_dict))
+    print("Limit:", limit)
+    summary = ""
+    for i in range(0, limit):
+        summary = summary + (docs[sorted_final[i][0]] + ". ")
+    #create output file to write the summary
+    out_filename = "C:/USC stuff/Applied NLP/Group Project/Test1/Sys2/abstract" + os.path.basename(filename)
+    print(out_filename)
+    output_file = open(out_filename, "w", encoding="latin1")
+    output_file.write(summary)
+    output_file.close()
+    
+
+my_directory = "C:/USC stuff/Applied NLP/Group Project/Test1/Docs"
+#search through the main directory for text files & execute 1st iteration
+for dirpath, dirnames, filenames in os.walk(my_directory):
+    for filename in filenames:
+        if mimetypes.guess_type(filename)[0] == 'text/plain':
+            file = os.path.join(dirpath, filename)
+            summarize_maxsim(file)
+
